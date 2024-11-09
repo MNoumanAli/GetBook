bfc7d54edd78e46246c03a0866b6b84ce98cec35
MNoumanAli <mnoumanali001@gmail.com>
2024-11-10 02:07:41 +0500

    setup db context and register service

---
+++ GetBook/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using GetBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetBook.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- GetBook/Program.cs
+++ GetBook/Program.cs
@@ -1,7 +1,13 @@
+using GetBook.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection"))
+    );
 
 var app = builder.Build();
 
