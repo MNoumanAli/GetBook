4dea13f7a15ea7ad39749b7edc790537a86363bd
MNoumanAli <mnoumanali001@gmail.com>
2024-11-11 00:44:33 +0500

    Update Navbar add create category functionality

---
+++ GetBook/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using GetBook.Data;
+using GetBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db) 
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _db.Category.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _db.Category.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
