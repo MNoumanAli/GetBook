fd86f3bb1d450776ec0bbcca8ec54e07befcf12b
MNoumanAli <mnoumanali001@gmail.com>
2024-11-22 23:52:50 +0500

    add migrations to modify product

---
+++ GetBook.DataAccess/Migrations/20241112200431_AddImaeURLToProduct.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GetBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImaeURLToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageURL",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Product");
+        }
+    }
+}
